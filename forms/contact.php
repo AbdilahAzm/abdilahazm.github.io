400b4720a6afdce55ab4262065d3029d6398698d
AbdilahAzm <abdilahazam.2021@student.uny.ac.id>
2021-09-28 10:15:27 +0700

    third commit

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'abdilahazam10@gmail.com';
+  $receiving_email_address = 'abdilahazam.2021@student.uny.ac.id';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
